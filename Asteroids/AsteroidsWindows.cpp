c6bcc76c7f417211c352e872d6434496c2eb1159
Erick Ruiz <erickblas.ruizponce4@gmail.com>
2022-06-23 19:31:05 -0500

    Set Initial Data

---
+++ Asteroids/AsteroidsWindows.cpp
@@ -0,0 +1,32 @@
+#include "Inih/cpp/INIReader.h"
+#include "Renderer.h"
+
+
+int main()
+{
+    INIReader ConFile("InitialData.ini");
+
+	if (ConFile.ParseError() < 0)
+		ConFile.PrintError("AsteroidsWindow");
+
+	Renderer Rend;
+
+
+    bool success = Rend.Initialize(ConFile.GetString("Window", "Name", "Error"),
+                                   ConFile.GetInteger("Window", "TopLeftXCoordinate", 100),
+                                   ConFile.GetInteger("Window", "TopLeftYCoordinate", 20),
+                                   ConFile.GetInteger("Window", "Width", 1080),
+                                   ConFile.GetInteger("Window", "Height", 700),
+                                   ConFile.GetInteger("Window", "Flags", 0),
+                                   ConFile.GetString("Window", "Font", "Error"));
+
+    if(success)
+    {
+        while(Rend.getIsRunning())
+        {
+            Rend.ProcessInput();
+            Rend.UpdateGame();
+            Rend.ClearRender();
+        }
+    }
+}
